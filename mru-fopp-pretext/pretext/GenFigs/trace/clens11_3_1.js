4e64a6cab6a902793ae279d8693c9c49a378dc1e
matthew-hre <whycardboard@gmail.com>
2023-04-27 17:09:20 -0600

    fixed indentation on every file i've ever touched. ugh.
    ctrl+k ctrl+shift+s to save a file WITHOUT FORMATTING

--- mru-fopp-pretext/pretext/GenFigs/trace/clens11_3_1.js
+++ mru-fopp-pretext/pretext/GenFigs/trace/clens11_3_1.js
@@ -1,4 +1,4 @@
 if (allTraceData === undefined) {
  var allTraceData = {};
  }
- allTraceData["clens11_3_1"] = {"code": "def hello2(s: str):\nprint(\"Hello \" + s)\nprint(\"Glad to meet you\")\n\nhello2(\"Iman\")\nhello2(\"Jackie\")\n\n", "trace": [{"event": "uncaught_exception", "line": 2, "offset": 5, "exception_msg": "IndentationError: expected an indented block (<string>, line 2)"}]}
+ allTraceData["clens11_3_1"] = {"code": "def hello2(s: str) -> None:\nprint(\"Hello \" + s)\nprint(\"Glad to meet you\")\n\nhello2(\"Iman\")\nhello2(\"Jackie\")\n\n", "trace": [{"event": "uncaught_exception", "line": 2, "offset": 5, "exception_msg": "IndentationError: expected an indented block (<string>, line 2)"}]}
--- mru-fopp-pretext/pretext/GenFigs/trace/clens11_3_2.js
+++ mru-fopp-pretext/pretext/GenFigs/trace/clens11_3_2.js
@@ -1,4 +1,4 @@
 if (allTraceData === undefined) {
  var allTraceData = {};
  }
- allTraceData["clens11_3_2"] = {"code": "def hello3(s: str, n: int):\ngreeting = f\"Hello {s} \"\nprint(greeting*n)\n\nhello3(\"Wei\", 4)\nhello3(\"\", 1)\nhello3(\"Kitty\", 11)\n\n", "trace": [{"event": "uncaught_exception", "line": 2, "offset": 8, "exception_msg": "IndentationError: expected an indented block (<string>, line 2)"}]}
+ allTraceData["clens11_3_2"] = {"code": "def hello3(s: str, n: int) -> None:\ngreeting = f\"Hello {s} \"\nprint(greeting*n)\n\nhello3(\"Wei\", 4)\nhello3(\"\", 1)\nhello3(\"Kitty\", 11)\n\n", "trace": [{"event": "uncaught_exception", "line": 2, "offset": 8, "exception_msg": "IndentationError: expected an indented block (<string>, line 2)"}]}
